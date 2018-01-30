76440cb284100d0968189ec8d499b139c71b644e
cherryhz <cherry@snarc.org>
2018-01-30 22:01:35 +0000

    Initial version which only read in one file and create one worksheet.

--- release-statistics/generateReleaseStatisticsDeviationReport.py
+++ release-statistics/generateReleaseStatisticsDeviationReport.py
@@ -6,18 +6,116 @@ __copyright__ = "Copyright 2018, The European Bioinformatics Institute (EMBL-EBI
 __credits__ = ["Cherry Hanquez"]
 __status__ = "Development"
 
+import sys
+import xlsxwriter
+import argparse
 
-# file arguments:
-# --prevStat	path to previous statistics report
-# --curStat	path to current statistics report
-# --outputFile	path to the output file
+#f1 = input ("Enter the first file name: ")
+#f2 = input ("Enter the second file name: ")
 
-# Requirements (can be done later on)
-# the script must check that:
-# * both input files: 
-# ** a) exist;
-# ** b) we have read access for them;
-# ** c) non-empty (let's say, at least 100 bytes);
-# * that output file does NOT exist;
-# * that we can write into suggested output location (e.g. by trying to create an empty output file first).
+# create a class for a typical section which contains header and data parts
+class Section:
+    def __init__(self, name):
+        self.name = name
+        self.headers = []
+        self.data = []
+    def append(self, line):
+        x = line.split()
+        data_start = 0
+        # treat the special case (those ends with "entries" in the file), mark
+        # it in such a way that data (aka number) starts after the name of system
+        while not x[data_start].endswith(":") and not x[data_start].isdecimal():
+            data_start += 1
 
+        # read the header once
+        if len(self.headers) == 0:
+            for i in x[data_start:]:
+                if not i.isdecimal():
+                    self.headers.append(i)
+
+        # data is in a format of list of tuples, each of which contains two lists of strings
+        numbers = []
+        for i in x[data_start:]:
+            if i.isdecimal():
+                numbers.append(i)
+        self.data.append((x[:data_start], numbers))
+
+# separate the file into sections whereas an empty line
+def parseSection(s):
+    dataLines = list()
+    while 1:
+        line = in_file.readline()
+        if not line or line == "\n":
+            break
+        dataLines.append(line)
+
+    if len(dataLines) == 0:
+        return None
+
+    s = Section(dataLines[0])
+    for l in dataLines[1:]:
+        s.append(l)
+    return s
+
+# convert data from [ ( [String], [String] ) ] -> [ [ String ] ]
+def convertList(listOfData):
+    l = []
+    for (name, numbers) in listOfData:
+        if len(name) > 1:
+            n = " ".join(name)
+        else:
+            n = name[0]
+        numbers.insert(0, n)
+        l.append(numbers)
+    return l
+
+class Writer:
+    def __init__(self, filename):
+        self.workbook = xlsxwriter.Workbook(filename)
+        self.worksheet = self.workbook.add_worksheet()
+        self.row = 0
+
+    def append(self, s):
+		# write headers
+        self.worksheet.write(self.row, 0, s.name)
+        for col in range(0, len(s.headers)-1):
+            self.worksheet.write(self.row, col + 1, s.headers[col])
+        # from the next row, write the data
+        self.row += 1
+        col = 0
+        for n in (convertList(s.data)):
+            for col in range(0, len(n)):
+                self.worksheet.write(self.row, col, n[col])
+            self.row += 1
+        self.row += 1
+
+    def close(self):
+        self.workbook.close()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--curStat', help="path to current statistics report")
+parser.add_argument('--outputFile', help="path to the output file")
+parser.add_argument('--prevStat', help="path to previous statistics report")
+
+args = parser.parse_args()
+
+print(args.curStat)
+
+with open(args.curStat, 'rt') as in_file:
+    # skip first 6 lines
+    for i in range(1,6):
+        in_file.readline()
+
+    # read data and write to excel
+    w = Writer(args.outputFile)
+    while True:
+        s = parseSection(in_file)
+        if s is None:
+            break
+        print("writing section " + s.name)
+        w.append(s)
+
+    # close file descriptor and writer
+    w.close()
+    in_file.close()
