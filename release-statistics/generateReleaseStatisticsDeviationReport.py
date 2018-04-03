1d5a75c628185aa0c5a331c39c03f7bd978aef97
Vladimir <vladimir@ebi.ac.uk>
2018-04-03 18:33:21 +0100

    code review: a) made arguments be non-optional (since the program cannot run w/o them) b) more pythonic variables naming. As per https://stackoverflow.com/questions/24180527/argparse-required-arguments-listed-under-optional-arguments / https://stackoverflow.com/a/24181138 arguments beginning with "--" are considered to be optional, therefore we cannot use them.

--- release-statistics/generateReleaseStatisticsDeviationReport.py
+++ release-statistics/generateReleaseStatisticsDeviationReport.py
@@ -6,51 +6,45 @@ __copyright__ = "Copyright 2018, The European Bioinformatics Institute (EMBL-EBI
 __credits__ = ["Cherry Hanquez"]
 __status__ = "Development"
 
-import sys
 import argparse
 
 from readData import Report
 from writeData import Writer
 
 # parsing argument
-parser = argparse.ArgumentParser()
-parser.add_argument('--curStat', help="path to current statistics report")
-parser.add_argument('--prevStat', help="path to previous statistics report")
-parser.add_argument('--outputFile', help="path to the output file")
+parser = argparse.ArgumentParser(description="AA Production Statistics Deviation Report")
+parser.add_argument('prev_stat', help="path to previous statistics report")
+parser.add_argument('cur_stat', help="path to current statistics report")
+parser.add_argument('output_file', help="path to the output file")
 
 # improve the args check so that:
 # if number of arguments is not what is expected (3), print help message for users
 # if len(sys.argv) == 1 or len(sys.argv) != 7
 
 args = parser.parse_args()
-def error_args(s):
-    print(s)
-    parser.print_help()
-    sys.exit(1)
+# def error_args(s):
+#     print(s)
+#     parser.print_help()
+#     sys.exit(1)
+#
+# if args.outputFile is None:
+#     error_args("Please type in the output file after \' --outputFile \'")
+#
+# if args.curStat is None:
+#     error_args("Please input the current statistics report after \' --curStat \'")
+#
+# if args.prevStat is None:
+#     error_args("Please input the previous statistics report after \' --prevStat \'")
 
-if args.outputFile is None:
-    error_args("Please type in the output file after \' --outputFile \'")
 
-if args.curStat is None:
-    error_args("Please input the current statistics report after \' --curStat \'")
-
-if args.prevStat is None:
-    error_args("Please input the previous statistics report after \' --prevStat \'")
-
-
-#print(len(sys.argv))
-#print(str(sys.argv))
-
-#w = Writer(pathNameCleanUp(args.outputFile))
-
-reportCur = Report(args.curStat)
-reportPrev = Report(args.prevStat)
+report_prev = Report(args.prev_stat)
+report_cur = Report(args.cur_stat)
 
 w = Writer(args.outputFile)
 # write deviation report to the 1st worksheet
-w.writeDiffReport(reportCur, reportPrev)
-w.writeReport(reportCur)
-w.writeReport(reportPrev)
+w.writeDiffReport(report_cur, report_prev)
+w.writeReport(report_cur)
+w.writeReport(report_prev)
 
 # close writer
 w.close()
