2358308f7e58308da9592695b8ce7689f839be44
Vladimir <vladimir@ebi.ac.uk>
2018-04-23 14:42:15 +0100

    introduced method to span a range plus tests for it

--- release-statistics/XlsUtilTest.py
+++ release-statistics/XlsUtilTest.py
@@ -1,14 +1,16 @@
 import unittest
 from xls_util import XlsUtil
 
+
 # this is the placeholder for the XlsUtil test
 # to make it run we need to set up packaging properly,
 # so that we could split the whole project into logical units
 class TestXlsUtilMethods(unittest.TestCase):
+
     def test_happy_path(self):
-        xlsUtil = XlsUtil()
-        self.assertEqual(xlsUtil.generate_deviations_sheet_name('report-2018_01.txt', 'report-2018_02.txt'), '2018_01-vs-2018_02')
-        print(xlsUtil.generate_deviations_sheet_name('report-2018_01.txt', 'report-2018_02.txt'))
+        xls_util = XlsUtil()
+        self.assertEqual(xls_util.generate_deviations_sheet_name('report-2018_01.txt', 'report-2018_02.txt'), '2018_01-vs-2018_02')
+        print(xls_util.generate_deviations_sheet_name('report-2018_01.txt', 'report-2018_02.txt'))
         # print(generateDifferencesSheetName('report-2018_01.txt', 'report-2018_02.txt'))
         # print(generateDifferencesSheetName('report-2018_01+++.txt', 'report-2018_02.txt'))
         # print(generateDifferencesSheetName('report-2018 _01.txt', 'report-2018_abc02.txt'))
@@ -16,12 +18,28 @@ class TestXlsUtilMethods(unittest.TestCase):
         # self.assertEqual(generateDifferencesSheetName('report-2018_01.txt', 'report-2018_02.txt'), 'FOO')
         pass
 
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
+    def test_range_name_zero_change(self):
+        xls_util = XlsUtil()
+        self.assertEqual(xls_util.span_range('D15'), 'D15:D15')
+
+    def test_range_col_increase(self):
+        xls_util = XlsUtil()
+        self.assertEqual(xls_util.span_range('M25', col_span=3), 'M25:O25')
+
+    def test_range_row_increase(self):
+        xls_util = XlsUtil()
+        self.assertEqual(xls_util.span_range('B8', row_span=3), 'B8:B10')
+
+    def test_column_exceeds_Z_row_stays(self):
+        xls_util = XlsUtil()
+        with self.assertRaises(Exception):
+            xls_util.span_range('Z3', col_span=2)
+
+    def test_column_exceeds_Z_row_increase(self):
+        xls_util = XlsUtil()
+        with self.assertRaises(Exception):
+            xls_util.span_range('Z3', col_span=2, row_span=4)
+
 
 if __name__ == '__main__':
     unittest.main()
--- release-statistics/xls_util.py
+++ release-statistics/xls_util.py
@@ -4,6 +4,10 @@ import re
 
 class XlsUtil:
     # VV: a bit of a sketch, needs testing (test cases defined in
+
+    def __init__(self):
+        self.regex_cell_ref = re.compile('^([A-Z])([0-9]+)$')
+
     def pathNameCleanUp(self, pathToFile):
         # strip out dir path and extension
         fileName = os.path.splitext(os.path.basename(pathToFile))[0]
@@ -37,3 +41,34 @@ class XlsUtil:
         #     fp1 = fp1[len(cmn_pref):]
         #     fp2 = fp2[len(cmn_pref):]
         return "{}-vs-{}".format(fp1, fp2)
+
+    def span_range(self, range_start, col_span=1, row_span=1):
+        range_start_parts = self.regex_cell_ref.match(range_start)
+
+        if not range_start_parts:
+            raise Exception('could not parse range start: {}'.format(range_start))
+
+        if col_span < 1:
+            raise Exception('colspan must be >=1. got: {}'.format(col_span))
+        if row_span < 1:
+            raise Exception('rowspan must be >=1. got: {}'.format(row_span))
+
+        col_name = range_start_parts.group(1)
+        # do a bit of validation:
+        if len(col_name) > 1:
+            raise Exception('method can work with single-char column name currently')
+        col_name = col_name.upper()
+        if (ord(col_name) < ord('A')) or (ord(col_name) > ord('Z')):
+            raise Exception('col name is expected to be within A-Z range. Got: {}'.format(col_name))
+
+        new_col_name_ord = ord(col_name) + col_span - 1 # minus one since our column already spans "1 column"
+        if (new_col_name_ord < ord('A')) or (new_col_name_ord > ord('Z')):
+            raise Exception('col name {} spanned by {} would fall outside of A-Z range'.format(col_name, col_span))
+        new_col_name = chr(new_col_name_ord)
+
+        row_num = int(range_start_parts.group(2))
+        new_row_num = row_num + row_span - 1 # minus one since our row already spans "1 row"
+
+        cells_range = '{}:{}{}'.format(range_start, new_col_name, new_row_num)
+        print('range start: {}, col_span: {}, row_span: {}-->{}'.format(range_start, col_span, row_span, cells_range))
+        return cells_range
